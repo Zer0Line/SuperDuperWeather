dca86ccf118f3a5df6bfd4704d2bb0f61eff8b6d
TT <timofeysin@gmail.com>
2019-01-31 23:50:07 +0300

    Lesson3

--- app/src/main/java/com/sinichkin/timofey/superduperweather/MainActivity.java
+++ app/src/main/java/com/sinichkin/timofey/superduperweather/MainActivity.java
@@ -1,18 +1,23 @@
 package com.sinichkin.timofey.superduperweather;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String tag = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(tag,"Прошли запуск активити");
         Button myOneButton = findViewById(R.id.buttonMoscow);
         myOneButton.setOnClickListener(onClickListener);
     }
@@ -20,7 +25,21 @@ public class MainActivity extends AppCompatActivity {
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            startActivity(new Intent(MainActivity.this, WheatherInMoscow.class));
+            Log.d(tag,"Переходим к запусу нового");
+            startAnotherActiv();
         }
     };
+
+    public void startAnotherActiv(){
+
+
+
+        Intent intent = new Intent(this,WheatherInMoscow.class);
+        String cytiName = "Москве";
+        intent.putExtra("CITY_NAME", cytiName);
+
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/sinichkin/timofey/superduperweather/WheatherInMoscow.java
+++ app/src/main/java/com/sinichkin/timofey/superduperweather/WheatherInMoscow.java
@@ -1,14 +1,25 @@
 package com.sinichkin.timofey.superduperweather;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class WheatherInMoscow  extends AppCompatActivity {
 
+
+
+    private static final String tag = MainActivity.class.getSimpleName();
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable  Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(tag,"Начинаем менять сет контент");
         setContentView(R.layout.wheatherinmoscow);
+        Intent intent = getIntent();
+        String cytiName = intent.getStringExtra("CITY_NAME");
+        TextView textView = findViewById(R.id.textViewWeathet);
+        textView.setText("В " +  cytiName+ " "+textView.getText());
     }
 }
