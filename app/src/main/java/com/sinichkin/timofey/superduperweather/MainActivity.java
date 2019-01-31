d2b09c0a413c3e338f9da40cf2b4d0116f7493ed
TT <timofeysin@gmail.com>
2019-01-31 17:32:50 +0300

    Lesson3

---
+++ app/src/main/java/com/sinichkin/timofey/superduperweather/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sinichkin.timofey.superduperweather;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+        Button myOneButton = findViewById(R.id.buttonMoscow);
+        myOneButton.setOnClickListener(onClickListener);
+    }
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(MainActivity.this, WheatherInMoscow.class));
+        }
+    };
+}
